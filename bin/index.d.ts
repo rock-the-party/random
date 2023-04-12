6582e94d1993f4eeeeca3123944a9bdd086af848
justplainfred <justplainfred@gmail.com>
2023-04-12 13:09:04 -0600

    updtes build procress and removes class

---
+++ bin/index.d.ts
@@ -0,0 +1,8 @@
+export interface IPoint {
+    x: number;
+    y: number;
+}
+export declare function NextDouble(): number;
+export declare function IntInRange(min: number, max: number): number;
+export declare function PointOnCircle(): IPoint;
+export declare function NextGaussian(mean: number, stdDev: number): number;
--- index.test.ts
+++ index.test.ts
@@ -1,13 +1,12 @@
-import { Random } from './index';
 import './__test__/custom-matchers'
 
-describe('Random', () => {
-  let random: Random;
+import { IntInRange, NextDouble, NextGaussian, PointOnCircle } from "./index";
 
+describe('Random', () => {
   describe('nextDouble', () => {
     it('returns a number between 0 and 1', () => {
       for (let i = 0; i < 1000; i++) {
-        let result = Random.nextDouble();
+        let result = NextDouble();
         expect(result).toBeGreaterThanOrEqual(0);
         expect(result).toBeLessThanOrEqual(1);
       }
@@ -17,7 +16,7 @@ describe('Random', () => {
   describe('intInRange', () => {
     it('returns a number within the specified range', () => {
       for (let i = 0; i < 1000; i++) {
-        let result = Random.intInRange(1, 10);
+        let result = IntInRange(1, 10);
         expect(result).toBeGreaterThanOrEqual(1);
         expect(result).toBeLessThanOrEqual(10);
       }
@@ -27,7 +26,7 @@ describe('Random', () => {
   describe('pointOnCircle', () => {
     it('returns a point on the unit circle', () => {
       for (let i = 0; i < 1000; i++) {
-        let point = Random.pointOnCircle();
+        let point = PointOnCircle();
         expect(point.x * point.x + point.y * point.y).toBeCloseTo(1, 5);
       }
     });
@@ -36,7 +35,7 @@ describe('Random', () => {
   describe('nextGaussian', () => {
     it('returns a number with the specified mean and standard deviation', () => {
       for (let i = 0; i < 1000; i++) {
-        let result = Random.nextGaussian(0, 1);
+        let result = NextGaussian(0, 1);
         expect(result).toBeWithinRange(-4, 4);
       }
     });
